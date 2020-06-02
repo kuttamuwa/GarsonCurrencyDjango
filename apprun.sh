e110a7c7ff95186b556fa6754e33af2667907534
Umut Ucok <ucok.umut@gmail.com>
2020-06-02 18:02:12 +0300

    service info

---
+++ apprun.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo /home/umut/webvenv/bin/python C:\Users\umut\PycharmProjects\WebDumanDjango\manage.py runserver 0.0.0.0:6080
+echo "Uygulama calismaya basladi 'date'" > /home/umut/PythonProjects/DumanWebDataService/appservice.log
