a204640a033279f0cfb1cebe8a10cef52ada0122
Umut Ucok <ucok.umut@gmail.com>
2020-06-02 16:56:47 +0300

    proda aktarma

--- WebDumanDjango/settings.py
+++ WebDumanDjango/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'webdbdjango',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': 'NeDvEd.1190',
         'HOST': 'localhost',
         'PORT': '5432',
     }
