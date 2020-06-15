76ab4de9745713a09bf554dd80870e94599da928
umut ucok <ucok.umut@gmail.com>
2020-06-15 17:56:10 +0300

    stable i hop

--- WebDumanDjango/settings.py
+++ WebDumanDjango/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'webdbdjango',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': '!@#QWEasd&!@N@n0Xen0n@2o2o@!',
         'HOST': 'localhost',
         'PORT': '5432',
     }  
